4d11682a5124a43a8e620a45b8b13b87c8ce9038
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-02-24 16:35:22 +0100

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < num; i++) {
+        result += string;        
+    }
+    return result;
 };
+console.log(repeatString("Hey", 3));
+console.log(repeatString("hello", 10));
+console.log(repeatString("hi", 1));
 
 // Do not edit below this line
 module.exports = repeatString;
