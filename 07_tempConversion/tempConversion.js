40ab3296aa6a92e3435a7b1ab9f5f5f9c573d06c
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-02-26 17:55:31 +0100

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(val) {
+  const cFormula = (val - 32)/1.8;
+  return Math.round(cFormula * 10) / 10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(val1) {
+  const fFormula = (val1 * 1.8) + 32;
+  return Math.round(fFormula * 10) / 10;
 };
 
 // Do not edit below this line
