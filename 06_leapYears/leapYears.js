ddd21379398756b413d63b1f0a5a9db76d5b441d
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-02-26 17:37:10 +0100

    finished exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(val) {
+  const divisibleBy4 = val % 4 === 0;
+  const divisibleBy100 = val % 100 === 0;
+  const divisibleBy400 = val % 400 === 0;
 
+  if(divisibleBy4 && (!divisibleBy100 || divisibleBy400)) { 
+    return true;
+  } else {
+    return false;
+  } 
+  
 };
 
 // Do not edit below this line
