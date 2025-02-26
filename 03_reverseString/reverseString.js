3eb01867d550517d19737f2e98fcbb641527f117
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-02-26 15:36:16 +0100

    finished exercise 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(array) {
+    let array1 = array.split("");
+    array1.reverse();
+    return array1.join("");
 };
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
