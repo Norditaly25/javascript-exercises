f9e406882dac2b9e9a973e6f29b32b46e59ac0c6
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-03-14 20:46:56 +0100

    concluded exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const number = Number(n);
+    if (number === 0) return 0;
+    if (number === 1) return 1;
+    if (number < 0) return `OOPS`;
+    let prev = 0, current = 1;
 
+    for (let i = 2; i <= number; i++) {
+        let next = prev + current;
+        prev = current;
+        current = next; 
+    }
+    return current;
 };
 
 // Do not edit below this line
