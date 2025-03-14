dd8aba732ca01f798639da9c9b038f5e0deb3ade
NordItaly25 <197090171+Norditaly25@users.noreply.github.com>
2025-03-14 18:34:31 +0100

    concluded exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (p) {
+    const array = p.toLowerCase().split(``).filter(char => char.match(/[a-z0-9]/));
+    const reversedArray = [...array].reverse();
+    return array.join(``) === reversedArray.join(``);
 };
 
 // Do not edit below this line
